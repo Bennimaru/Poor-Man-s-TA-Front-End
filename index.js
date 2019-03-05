e5bad74742ed3c90fea53800983aebdae823b563
Bennimaru <bennywhyu@gamil.com>
2019-03-05 17:10:32 -0500

    added sidebar fetch JS

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
     $('#sidebar').toggleClass('active');
   });
 });
+
+fetchBoroughs()
+
+function fetchBoroughs(){
+ fetch('http://localhost:3000/boroughs')
+ .then(resp => resp.json())
+ .then(json => renderBoroughs(json))
+}
+
+function renderBoroughs(json){
+  const sidenav = document.querySelector('#sidebar-list')
+  json.forEach(function(borough){
+  const boroughName = document.createElement('li')
+    boroughName.innerText = borough.name
+    boroughName.id = borough.id
+    sidenav.append(boroughName)
+  })
+}
