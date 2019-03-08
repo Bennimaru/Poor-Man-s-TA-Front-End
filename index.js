891b2a23fde008189cbdd0a5f7aee8e16ed8502b
Bennimaru <bennywhyu@gamil.com>
2019-03-08 11:42:11 -0500

    added edit feature

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 console.log("hi");
 
-
 $(document).ready(function(){
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
@@ -12,11 +11,18 @@ const boroughURL = "http://localhost:3000/boroughs"
 const locationsURL = "http://localhost:3000/locations"
 const locationContainer = document.querySelector("#locationContainer")
 const singleLocation = document.querySelector('#singleLocation')
+const formLocation = document.querySelector("#formLocation")
 const sidenav = document.querySelector('#sidebar-list')
+const addForm = document.querySelector('.add-form')
+const toggleForm = document.querySelector('.toggleForm')
 const backButton = document.querySelector("#back")
 const forwardButton = document.querySelector("#forward")
 
 fetchBoroughs()
+sidenav.addEventListener("click",fetchLocations)
+locationContainer.addEventListener("click",fetchSingleLocation)
+formLocation.addEventListener("submit",editLocationDetails)
+addForm.addEventListener("submit", createPlace)
 
 function fetchBoroughs(){
  return fetch(boroughURL)
@@ -33,11 +39,11 @@ function renderBoroughs(json){
   })
 }
 
-sidenav.addEventListener("click",fetchLocations)
-
 function fetchLocations(event){
   event.preventDefault()
   locationContainer.innerHTML=""
+  singleLocation.innerHTML=""
+  formLocation.innerHTML=""
   const clickedBorough = event.target.id
   return fetch(boroughURL+`/${clickedBorough}`)
     .then(res => res.json())
@@ -59,8 +65,6 @@ function renderLocations(locations){
   })
 }
 
-locationContainer.addEventListener("click",fetchSingleLocation)
-
 function fetchSingleLocation(event){
   event.preventDefault()
   singleLocation.innerHTML=''
@@ -77,12 +81,30 @@ function renderSingleLocation(location){
   const singleLocationAddress = document.createElement("p")
   const singleLocationDesc = document.createElement("p")
   singleLocationImage.src = location.image
-  singleLocationName.innerText = location.name
+  singleLocationName.innerText = `Name: ${location.name}`
   singleLocationImage.className = "singleLocationImage"
   singleLocationAddress.innerText = `Address: ${location.address}`
   singleLocationDesc.innerText = `Description: ${location.description}`
   singleLocationCard.append(singleLocationImage,singleLocationName,singleLocationAddress,singleLocationDesc)
   singleLocation.append(singleLocationCard)
+
+  formLocation.innerHTML= `
+  <form action="PATCH" class="edit-form">
+    <div class="form-group">
+      <input type="text" name="name" class="form-control" value="${location.name}">
+      <input type="text" name="address" class="form-control" value="${location.address}">
+      <input type="text" name="image" class="form-control" value=${location.image}>
+      <input type="text" name="description" class="form-control" value="${location.description}">
+      <select data-location-id=${location.id}>
+        <option value="1">Bronx</option>
+        <option value="2">Brooklyn</option>
+        <option value="3">Manhattan</option>
+        <option value="4">Queens</option>
+        <option value="5">Staten Island</option>
+      </select>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>`
 }
 
 forwardButton.addEventListener("click",showNextFive)
@@ -105,9 +127,6 @@ function showLastFive(){
 //     .then(locations => locations.forEach(showLocation))
 }
 
-let addForm = document.querySelector('.add-form')
-addForm.addEventListener("submit", createPlace)
-let toggleForm = document.querySelector('.toggleForm')
 toggleForm.addEventListener('click', (event) => {
   if (addForm.style.display === "none"){
     addForm.style.display = "block";
@@ -117,9 +136,7 @@ toggleForm.addEventListener('click', (event) => {
 })
 
 function createPlace(event){
-  debugger
   event.preventDefault()
-  console.log(event)
   let newData = {
     name: event.target[0].value,
     address: event.target[1].value,
@@ -127,7 +144,7 @@ function createPlace(event){
     description: event.target[3].value,
     borough_id: event.target[4].value
   }
-  fetch("http://localhost:3000/locations", {
+  fetch(locationsURL, {
     method: "POST",
     headers: {
       'Content-type': 'application/json',
@@ -135,4 +152,25 @@ function createPlace(event){
     },
     body: JSON.stringify(newData)
   })
+}
+
+function editLocationDetails(event){
+  event.preventDefault()
+  let locationId = event.target[4].dataset.locationId
+  let editedData = {
+    name: event.target[0].value,
+    address: event.target[1].value,
+    image: event.target[2].value,
+    description: event.target[3].value,
+    borough_id: event.target[4].value
+  }
+  fetch(locationsURL+`/${locationId}`,{
+    method: "PATCH",
+    headers:{
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body:JSON.stringify(editedData)
+  })
+  .then(console.log("hi"))
 }
